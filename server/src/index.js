06e2a589507144a47da9ce1c70497e0386009aea
helloaaditya <aadityakum123@gmail.com>
2024-05-29 02:13:02 +0530

    bshd

--- server/src/index.js
+++ server/src/index.js
@@ -20,20 +20,20 @@ app.use(
     },
   })
 );
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
 // Define routes below
 app.get("/", (req, res) => {
-  res.send("<h1>👋🏻 Hello from the Lern server!</h1>");
+    res.send("<h1>👋🏻 Hello from the Lern server!</h1>");
 });
 
 app.use("/api", routes);
 
 connectToDatabase().then(() => {
-  app.listen(port, () => {
-    console.log(`🗄️  Express server listening on port ${port}`);
-  });
+    app.listen(port, () => {
+        console.log(`🗄️  Express server listening on port ${port}`);
+    });
 });
